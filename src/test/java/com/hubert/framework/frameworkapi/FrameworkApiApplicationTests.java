9a91dacf79c6620345118455b45bc969dce33405
zhengxiaobin <xiaobin.zheng@hand-china.com>
2017-07-17 16:10:01 +0800

    first commit

---
+++ src/test/java/com/hubert/framework/frameworkapi/FrameworkApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hubert.framework.frameworkapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrameworkApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
